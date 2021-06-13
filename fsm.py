e847168ed2788bacc6053161c97ac458a3514fa5
Mark Johnson <softwareapocolypse@gmail.com>
2021-06-13 10:36:22 -0500

    State labels are now centered.

--- fsm.py
+++ fsm.py
@@ -3,21 +3,23 @@ from dearpygui.simple import *
 
 doubleClickTimer = 0
 radius = 50
-#circles = [ [1*radius, radius], [3*radius, radius], [5*radius, radius], [7*radius, radius] ]
-#circles = [ [1*radius, radius, None], [3*radius, radius, None], [5*radius, radius, None], [7*radius, radius, None] ]
-circles = [ [1*radius, radius, None, None], [3*radius, radius, None, None], [5*radius, radius, None, None], [7*radius, radius, None, None] ]
+bubbleColor = [ 255, 255, 255, 255 ]    # Bubble border color.
+dragging = False                        # No drag at init.
+target = None                           # Store the target tag while dragging.
+fontsize = [ 6, 14 ]                    # This is a guess used to center the drawn text inside the state bubble.  (size=12)
 
-white = [ 255, 255, 255, 255 ]
-
-dragging = False 
-target = ""
-
+                                        # BubbleX, BubbleY, BubbleTag, LabelTag, LabelXOffset, LabelYOffset
+bubbleInfo = [ 
+    [1*radius, radius, None, None, None, None], 
+    [3*radius, radius, None, None, None, None], 
+    [5*radius, radius, None, None, None, None], 
+    [7*radius, radius, None, None, None, None] ]
 
 def main_callback(sender, data):
     global doubleClickTimer
     global radius
     global dragging
-    global white
+    global bubbleColor
     global target
     
     if is_mouse_button_dragging(mvMouseButton_Left, 10):
@@ -34,15 +36,16 @@ def main_callback(sender, data):
         
         # Find the closest state position to the mouse click using (mx - px)^2 + (my - py)^2.
         closest = float("inf")
-        for index, value in enumerate(circles):
+        for index, value in enumerate(bubbleInfo):
             distance = pow((mx - value[0]), 2) + pow((my - value[1]), 2)
             print(value[2] + ": " + str(distance))
             if closest > distance:
                 closest = distance          # If this is closer, save the distance.
                 target = index              # If this is closer, save the index.
                 
-        print("Target = " + str(target) + ", distance = ", str(distance) +", nameID=" + circles[target][3])
+        print("Target = " + str(target) + ", distance = ", str(distance) +", nameID=" + bubbleInfo[target][3])
         dragging = True
+        print(bubbleInfo[index])
         
     if is_key_down(mvKey_Shift) and is_mouse_button_clicked(mvMouseButton_Left):
         set_value("Shift + Left Mouse Clicked", "True")
@@ -62,42 +65,50 @@ def main_callback(sender, data):
             set_value("Left Mouse Double Clicked", "False")
             
     if dragging:
+        # Move the State Bubble.
         mouse = get_mouse_pos()
-        modify_draw_command("drawing##widget", circles[target][2], center=mouse)
-        modify_draw_command("drawing##widget", circles[target][3], pos=mouse)
-        circles[target][0] = mouse[0]
-        circles[target][1] = mouse[1]
+        modify_draw_command("drawing##widget", bubbleInfo[target][2], center=mouse)
+        bubbleInfo[target][0] = mouse[0]
+        bubbleInfo[target][1] = mouse[1]
         
+        # Move the State Name Text.  Note that we have to apply the stored offset to center.
+        position = [0,0]
+        position[0] = mouse[0] - bubbleInfo[target][4]
+        position[1] = mouse[1] - bubbleInfo[target][5]
+        modify_draw_command("drawing##widget", bubbleInfo[target][3], pos=position)
 
 
 with window("Main Window"):
-
+    
+    # Create the drawing area.
     winSize = get_main_window_size()
-    #winSize = [800, 600]
-    #add_drawing("drawing##widget", width=800, height=500)
-    #draw_rectangle("drawing##widget", [0, 500], [800, 0], [255, 0, 0, 255], fill=[0, 0, 25, 255], rounding=12, thickness=1.0)
-    print(str(winSize[0]) + ", " + str(winSize[1]))
     add_drawing("drawing##widget", width=winSize[0], height=winSize[1])
-    
     draw_rectangle("drawing##widget", [0, winSize[1]], [winSize[0], 0], [255, 0, 0, 255], fill=[0, 0, 25, 255], rounding=12, thickness=1.0)
-    #draw_circle("drawing##widget", circles[0], radius, white, tag="movingCircle", thickness=2.0)
     
+    # Initialize the visual objects.
     id = 0
-    for state in circles:
+    for state in bubbleInfo:
         #print(state)
         id = id + 1
         stateTag = "StateTag" + str(id)
         nameTag = "NameTag" + str(id)
-        center = state[0:2]
-        #print(stateTag)
-        #print(center)
-        draw_circle("drawing##widget", center, radius, white, tag=stateTag, thickness=2.0)
-        draw_text("drawing##widget", center, str(id), color=white, tag=nameTag, size=12)
+        
         state[2] = stateTag
         state[3] = nameTag
+        state[4] = (fontsize[0] * len(nameTag)) / 2
+        state[5] = fontsize[1] / 2
         
+        center = state[0:2]
+        draw_circle("drawing##widget", center, radius, bubbleColor, tag=stateTag, thickness=2.0)
+        
+        position = center
+        position[0] = position[0] - state[4]
+        position[1] = position[1] - state[5]
+        draw_text("drawing##widget", position, nameTag, color=bubbleColor, tag=nameTag, size=12)
+
+    # Set the graphic renderer callback to support moving the visual objects.
     set_render_callback(main_callback)
     
-    
+# Start the framework.
 start_dearpygui(primary_window="Main Window")
 
